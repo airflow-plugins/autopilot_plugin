e0f97554da9e4fa88550634f6c1407b9305ba91b
Benjamin Gregory <benjaminrgregory@gmail.com>
2018-03-01 11:32:08 -0700

    remove unused methods in hook

--- hooks/autopilot_hook.py
+++ hooks/autopilot_hook.py
@@ -4,26 +4,12 @@ from airflow.hooks.http_hook import HttpHook
 class AutopilotHook(HttpHook):
     def __init__(self, method='GET', http_conn_id='autopilot_conn'):
         super().__init__(method, http_conn_id)
-        self.http_conn_id = http_conn_id
-        self.headers = ''
 
     def get_conn(self, headers=None):
         conn = super().get_connection(self.http_conn_id)
-        self.headers = {'autopilotapikey': conn.extra_dejson.get('key'),
+        self.headers = {'autopilotapikey': conn.extra_dejson.get('api_key'),
                         'accept-encoding': 'application/json'}
+
         session = super().get_conn(self.headers)
 
         return session
-
-    def run(self,
-            endpoint,
-            data=None,
-            headers=None,
-            extra_options=None):
-        return super().run(endpoint, data, headers, extra_options)
-
-    def get_pandas_df(self, sql):
-        pass
-
-    def get_records(self, sql):
-        pass
